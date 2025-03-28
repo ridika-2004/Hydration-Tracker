47077b5fb5272fa43a1b24f38b3eba01bd6edd72
ridika-2004 <nazninridika2004@gmail.com>
2025-03-28 14:19:48 +0600

    added userexists method

--- src/App/App.java
+++ src/App/App.java
@@ -1,23 +1,32 @@
 package App;
 
+import java.time.LocalDate;
+
+import Dashboard.UserInteraction;
+import Utils.FileUtils;
+import Utils.TrackerUtils;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println(
-        "                                                            ╔═════════════════════════════════════╗\n" +
-        "                                                            ║           CHOOSE YOUR ROLE          ║\n" +
-        "                                                            ╠═════════════════════════════════════╣\n" +
-        "                                                            ║          [1] Regular User           ║\n" +
-        "                                                            ║          [2] Organizer              ║\n" +
-        "                                                            ║          [3] Exit                   ║\n" +
-        "                                                            ╚═════════════════════════════════════╝");
+        // new UserInteraction().userDashboard();
+        LocalDate todaydate = TrackerUtils.formatStringToDate("28/03/25");
+        System.out.println(!todaydate.equals(LocalDate.now()));
+        // System.out.println(
+        // "                                                            ╔═════════════════════════════════════╗\n" +
+        // "                                                            ║           CHOOSE YOUR ROLE          ║\n" +
+        // "                                                            ╠═════════════════════════════════════╣\n" +
+        // "                                                            ║          [1] Regular User           ║\n" +
+        // "                                                            ║          [2] Organizer              ║\n" +
+        // "                                                            ║          [3] Exit                   ║\n" +
+        // "                                                            ╚═════════════════════════════════════╝");
 
-        System.out.println("████████████████████████\r\n" + //
-                        "█    CHOOSE OPTION    █\r\n" + //
-                        "████████████████████████\r\n" + //
-                        "█  [1] Start         █\r\n" + //
-                        "█  [2] Settings      █\r\n" + //
-                        "█  [3] Exit          █\r\n" + //
-                        "████████████████████████\r\n" + //
-                        "");
+        // System.out.println("████████████████████████\r\n" + //
+        //                 "█    CHOOSE OPTION    █\r\n" + //
+        //                 "████████████████████████\r\n" + //
+        //                 "█  [1] Start         █\r\n" + //
+        //                 "█  [2] Settings      █\r\n" + //
+        //                 "█  [3] Exit          █\r\n" + //
+        //                 "████████████████████████\r\n" + //
+        //                 "");
     }
 }
--- src/Dashboard/UserInteraction.java
+++ src/Dashboard/UserInteraction.java
@@ -1,12 +1,50 @@
 package Dashboard;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import Utils.FileUtils;
 import Utils.TrackerUtils;
 
 public class UserInteraction {
-    public void UserDashboard(){
+
+    private static final String filepath = "src/Txt_Files/water_stats.txt";
+    public void userDashboard(){
         String name = TrackerUtils.takeInput("Enter your name : ");
         String email = TrackerUtils.takeInput("Enter your email : ");
+
+        if(userExists(email)){
+            //
+        } else {
+            //
+        }
     }
 
-    
+    private void haveToEnterDetails(){
+        //
+    }
+
+    private boolean userExists(String email){
+        String userresult = FileUtils.searchFromFile(filepath, email);
+
+        String[] parts = userresult.split("\\|");
+        LocalDate userDate = TrackerUtils.formatStringToDate(parts[FileUtils.dateIndex]);
+        if(!userDate.equals(LocalDate.now())){
+            return false;
+        }
+        
+        LocalTime sleepTime = TrackerUtils.formatStringToTime(parts[FileUtils.sleepTimeIndex]);
+        if(sleepTime.isBefore(LocalTime.now())){
+            return false;
+        }
+
+        LocalTime wakeTime = TrackerUtils.formatStringToTime(parts[FileUtils.wakeTimeIndex]);
+        if(wakeTime.isBefore(LocalTime.now())){
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
 }
