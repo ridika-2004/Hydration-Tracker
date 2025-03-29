4047563359486de364896b9a1bb1beeccbffd947
ridika-2004 <nazninridika2004@gmail.com>
2025-03-29 15:14:28 +0600

    added reminder sound

--- src/Codes/Reminder.java
+++ src/Codes/Reminder.java
@@ -1,6 +1,11 @@
 package Codes;
+import java.io.File;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 public class Reminder {
     private Tracker tracker;
@@ -17,6 +22,7 @@ public class Reminder {
             public void run() {
                 if (tracker.needsReminder()) {
                     System.out.println("Reminder: Drink water!");
+                    playBeepSound();
                 }
             }
         }, 0, 60 * 60 * 1000);
@@ -24,5 +30,15 @@ public class Reminder {
 
     public void stopReminders() {
         reminderTimer.cancel();
+    }
+
+    private void playBeepSound() {
+        try {
+            File soundFile = new File("src\\sound\\reminder.wav");
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (Exception e) {}
     }
 }
